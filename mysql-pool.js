283351ed56089e60093228e4874d985300bea031
Atle Olsø <atle.olso@ntnu.no>
2022-06-24 17:22:00 +0200

    Update dependency versions + replace mysql with mysql2

--- mysql-pool.js
+++ mysql-pool.js
@@ -1,6 +1,6 @@
-import mysql from "mysql";
+import mysql2 from "mysql2";
 
-const pool = mysql.createPool({
+const pool = mysql2.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
